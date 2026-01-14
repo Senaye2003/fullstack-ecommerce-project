492513308597afe6e5480f90133a9b74c1a406ba
senaye <sweldebe@charlotte.edu>
2026-01-13 21:18:49 -0500

    some code cleanup

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -10,8 +10,8 @@ function App() {
   const [cart, setCart] = useState([]);
 
   const loadCart = async () => {
-      const response = await axios.get('/api/cart-items?expand=product');
-      setCart(response.data);
+    const response = await axios.get('/api/cart-items?expand=product');
+    setCart(response.data);
   };
 
   useEffect(() => {
@@ -21,7 +21,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
-      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart = {loadCart}/>} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
     </Routes>
   )
